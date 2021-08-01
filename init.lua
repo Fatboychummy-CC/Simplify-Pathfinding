6ffe9df4b37ae0d707da4bb90d2731d83d63a9d0
fatboy <fatboychummy@gmail.com>
2021-07-31 22:49:46 -0700

    Remove that "old mc version handling"
    I can't guarantee it will work on older versions.

--- init.lua
+++ init.lua
@@ -1,20 +1,10 @@
 ---
 
--- This block should allow this api to work on all versions of minecraft, regardless of expect existing.
 local ok, expect = pcall(require, "cc.expect")
 if ok then
   expect = expect.expect
 else
-  expect = function(argn, arg, ...)
-    local types = table.pack(...)
-    local argtype = type(arg)
-    for i = 1, types.n do
-      if types[i] == argtype then
-        return
-      end
-    end
-    error(string.format("Bad argument #%d: Expected %s, got %s.", argn, table.concat(types, "/"), argtype), 3)
-  end
+  error("This module will only work on CC:Tweaked for minecraft 1.12.2+")
 end
 
 local a = {}
