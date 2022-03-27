64809122ef88fb7cecbbd6982d8dbdb2b488da40
Matthew W <fatboychummy@gmail.com>
2022-03-27 17:37:01 -0600

    iteratePath body. Unsure if it works, will need to test.

--- Path.lua
+++ Path.lua
@@ -33,10 +33,79 @@ end
 -- This allows you to use a for loop and complete some action for every movement.
 -- @tparam table path The path to follow.
 -- @tparam table tu The turtle object to use. This expects a turtle object in the format of the TrackingTurtle, which has the extra methods '.getPosition()' and '.getFacing()'
+-- @tparam number start The index to start movement at. Useful if you need to continue following a path after aborting for whatever reason.
 -- @treturn function(boolean, boolean, boolean) Iterator which will move the turtle along the path, then return after each move.
-function Path.iteratePath(path, tu)
+function Path.iteratePath(path, tu, start)
   expect(1, path, "table")
   expect(2, tu, "table")
+  expect(3, start, "number")
+
+  local i = 0
+  --- Iterator function
+  -- @tparam canDig Whether the turtle can dig blocks.
+  -- @tparam canAttack Whether the turtle can attack mobs in the way.
+  -- @tparam canGoBack Whether to optimize movement so that the turtle can move backwards too.
+  return function(canDig, canAttack, canGoBack)
+    i = i + 1
+    if not path[i] then return end -- detect the end of the path.
+
+    -- collect data about the turtle's current position and facing, as well as direction needed.
+    local facing = tu.getFacing()
+    local cX, cY, cZ = tu.getPosition()
+    local nX, nY, nZ = table.unpack(path[i], 1, 3)
+    local neededFacing = tu.getFacingToBlock(cX, cY, cZ, nX, nY, nZ)
+    local movement = tu.forward
+
+    -- if we can move backwards, do that.
+    if canGoBack and facing == (neededFacing + 2) % 4 then
+      movement = tu.back
+    else
+      -- if we can't go backwards or are not facing it properly, turn to it.
+      tu.face(neededFacing)
+    end
+
+    for i = 1, 100 do
+      -- Attempt to move.
+      if movement() then
+        return true, {nX, nY, nZ}
+      end
+
+      -- If we could not move, try attacking.
+      if canAttack then
+        -- if we were moving backwards along the path
+        if canGoBack and facing == (neededFacing + 2) % 4 then
+          -- Face the correct direction
+          tu.face(neededFacing)
+          facing = tu.getFacing()
+
+          -- Set the new movement to be facing where we are going.
+          movement = tu.forward
+        end
+        tu.attack()
+      end
+
+      -- If we could not move, also try digging.
+      if canDig then
+        -- if we were moving backwards along the path
+        if canGoBack and facing == (neededFacing + 2) % 4 then
+          -- Face the correct direction
+          tu.face(neededFacing)
+          facing = tu.getFacing()
+
+          -- Set the new movement to be facing where we are going.
+          movement = tu.forward
+        end
+        tu.dig()
+      end
+
+      -- hopefully we can just wait it out if something is blocking us... right?
+      if not canAttack and not canDig then
+        os.sleep(1)
+      end
+    end
+
+    return false, "Failed to move."
+  end
 end
 
 return Path
