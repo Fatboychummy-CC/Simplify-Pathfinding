6f9267a0b6f8095676a97b5eece7fe04ee490566
Matthew W <fatboychummy@gmail.com>
2022-03-27 15:33:37 -0600

    Add a list of some blocks which don't hamper movement.

---
+++ BlocksDontStopMovement.lua
@@ -0,0 +1,12 @@
+return {
+  blocks = {
+    ["minecraft:water"] = true,
+    ["minecraft:seagrass"] = true,
+    ["minecraft:tall_seagrass"] = true,
+    ["minecraft:snow"] = true,
+    ["minecraft:lava"] = true
+  },
+  tags = {
+    ["minecraft:replaceable_plants"] = true
+  }
+}
