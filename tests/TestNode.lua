83294840fae326dadfdf6deadc72c20601e7163f
Matthew W <fatboychummy@gmail.com>
2022-04-04 23:24:47 -0600

    Separate each test with two line breaks so as to make looky looky easier

--- tests/TestNode.lua
+++ tests/TestNode.lua
@@ -1,17 +1,23 @@
 local Node = require "SimplifyPathfinding.Node"
 
 local suite = cctest.newSuite "TestNode"
+
+
   "NODE_CREATE_NO_ERROR" (function()
     EXPECT_NO_THROW(Node.create, 1, 1, 1)
     EXPECT_NO_THROW(Node.create, -1, -1, -1)
     EXPECT_NO_THROW(Node.create, -1, 1, -1)
     EXPECT_NO_THROW(Node.create, -41015, 258478, -1111111)
   end)
+
+
   "CREATE_EXPECTATIONS" (function()
     EXPECT_THROW_ANY_ERROR(Node.create, 1, 1, nil)
     EXPECT_THROW_ANY_ERROR(Node.create, 1, nil, 1)
     EXPECT_THROW_ANY_ERROR(Node.create, nil, 1, 1)
   end)
+
+
   "CREATE_DATA" (function()
     local x, y, z = 1, 5, 9
     local node = Node.create(x, y, z)
